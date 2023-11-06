d1050a58801a013caca13d02fe8e9adbee23c57b
sam morales <smorales40@cnm.edu>
2023-11-06 11:02:27 -0700

    Added onto StudentRepository.

--- app/src/main/java/edu/cnm/deepdive/zoomattendance/service/StudentRepository.java
+++ app/src/main/java/edu/cnm/deepdive/zoomattendance/service/StudentRepository.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.zoomattendance.service;
 import androidx.lifecycle.LiveData;
 import edu.cnm.deepdive.zoomattendance.model.dao.StudentDao;
 import edu.cnm.deepdive.zoomattendance.model.entity.Student;
+import io.reactivex.rxjava3.core.Single;
 import java.util.List;
 import javax.inject.Inject;
 
@@ -16,5 +17,10 @@ public class StudentRepository {
 
   public LiveData<List<Student>> search (String fragment) {
     return studentDao.search("%" + fragment + "%");
+  }
+
+  private Single<Student> insert(Student student) {
+  return studentDao
+      .insert(student);
   }
 }
